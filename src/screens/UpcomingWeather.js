a74de1794a376adbc28163e18ce95da33a84f0ff
n-buckley <buckley.ni@northeastern.edu>
2023-06-20 17:35:52 -0400

    ref: list to listitem component

--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -1,18 +1,6 @@
 import React from "react" 
-import { SafeAreaView, View, Text, StyleSheet, FlatList, StatusBar, ImageBackground} from 'react-native'
-import { Feather} from '@expo/vector-icons'
-
-const Item = (props) => {
-    const { dt_txt, min, max, consition} = props
-    return (
-        <View style={styles.Item} >
-            <Feather name="sun" size={50} color='white' />
-            <Text style={styles.itemDate}>{dt_txt}</Text>
-            <Text style={styles.itemTemp}>{min}</Text>
-            <Text style={styles.itemTemp}>{max}</Text>
-        </View>
-    )
-}
+import { SafeAreaView, Text, StyleSheet, FlatList, StatusBar, ImageBackground} from 'react-native'
+import ListItem from '../components/ListItem'
 
 export const UpcomingWeather = () => {
     const DATA = [
@@ -54,7 +42,7 @@ export const UpcomingWeather = () => {
         }, 
     ]
     const renderItem = ({item}) => (
-        <Item 
+        <ListItem 
             condition={item.weather[0].main}
             dt_txt={item.dt_txt}
             min={item.main.temp_min}
@@ -84,28 +72,6 @@ const styles = StyleSheet.create({
         marginTop: StatusBar.currentHeight || 0,
         backgroundColor: 'lightblue',
       }, 
-
-    Item: {
-        padding: 20, 
-        marginVertical: 8,
-        marginHorizontal: 16, 
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center', 
-        borderWidth: 5,
-        backgroundColor: 'pink',
-    },
-
-    item_temp: {
-        color: 'white',
-        fontSize: 20,
-    },
-
-    item_date: {
-        color: 'white',
-        fontSize: 15,
-    },
-
     ImageBackground: {
         flex: 1,
     },
