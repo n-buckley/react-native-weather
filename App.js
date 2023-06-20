aecfeddb3658fe48f7c86b75a1de1f18697a921c
n-buckley <buckley.ni@northeastern.edu>
2023-06-20 02:37:36 -0400

    add: basic styles

--- App.js
+++ App.js
@@ -3,26 +3,47 @@ import { Text, View, SafeAreaView, StyleSheet} from 'react-native'
 
 const App = () => {
   return (
-    <SafeAreaView style= {styles.wrapper}>
-      <View style={styles.container}>
+    <SafeAreaView style= {styles.view__safeArea}>
+      <View style={styles.view__appContainer}>
         <Text>Current Weather</Text>
-        <Text>6</Text>
-        <Text>Feels lik: 5</Text>
-        <Text>High: 8 Low: 6</Text>
+        <Text style={styles.text__temp}>6</Text>
+        <Text style={styles.text__feels}>Feels like: 5</Text>
+        <View style={styles.view__wrapperHighLow}> 
+          <Text style={styles.text__highLow}>High: 8</Text>
+          <Text style={styles.text__highLow}>Low: 6</Text>
+        </View>
       </View>
     </SafeAreaView>
   )
 } 
 
 const styles = StyleSheet.create({
-  wrapper: {
+  view__safeArea: {
     flex: 1
   }, 
-  container: { 
+  view__appContainer: { 
     backgroundColor: 'pink',
     flex: 1,
     alignItems: 'center',
-  }
+  },
+
+  text__temp: {
+    color: 'black',
+    fontSize: 48
+  }, 
+
+   text__feels: {
+    fontSize: 30, 
+    color: 'black'
+   },
+
+   text__highLow: {
+    fontSize: 20, 
+    color: 'black'
+   }, 
+   view__wrapperHighLow: {
+    flexDirection: 'row',
+   },
 })
 
 export default App
