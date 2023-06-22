2ed0c3dfae2f3c1efd9c67f3bcc22ccca03ad490
n-buckley <buckley.ni@northeastern.edu>
2023-06-21 22:48:51 -0400

    add: activity indicator conditional rendering

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { View, ActivityIndicator, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
-import Counter from './src/demonstration/Counter'
 
 const App = () => {
   //TODO: can access the platform the user is using to do conditional Top or Bottom navigation
+
+  const [loading, setLoading] = useState(true)
+  //TODO: theres a couple ways to do "conditional rendering"
+  if (loading) {
+    return (
+      <View style={styles.IndicatorWrapper}>
+        <ActivityIndicator size={'large'} color={'blue'} />
+      </View>
+    )
+  }
   return (
     <NavigationContainer>
-      {/* <Tabs /> */}
-      <Counter />
+      <Tabs />
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  IndicatorWrapper: {
+    justifyContent: 'center',
+    flex: 1
+  }
+})
 
 export default App
