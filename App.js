64088b136fdd18f2ebe66da041775e010b249dc4
n-buckley <buckley.ni@northeastern.edu>
2023-06-22 00:13:27 -0400

    add: hooks for user location

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, ActivityIndicator, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
+import * as Location from 'expo-location'
+
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 const App = () => {
   //TODO: can access the platform the user is using to do conditional Top or Bottom navigation
 
   const [loading, setLoading] = useState(true)
+  const [location, setLocation] = useState(null)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    ;(async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync
+      if (status !== 'granted') {
+        setError('permission to access location was denied')
+        return
+      }
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location)
+    })()
+  }, [])
+
+  if (location) {
+    console.log(location)
+  }
+
   //TODO: theres a couple ways to do "conditional rendering"
   if (loading) {
     return (
@@ -15,6 +37,7 @@ const App = () => {
       </View>
     )
   }
+
   return (
     <NavigationContainer>
       <Tabs />
