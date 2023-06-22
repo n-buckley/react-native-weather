3359104367729b8ee25fa5c4b540076fdc7f9c40
n-buckley <buckley.ni@northeastern.edu>
2023-06-21 21:32:27 -0400

    add: static counter example (stateless)

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
+import Counter from './src/demonstration/Counter'
 
 const App = () => {
   //TODO: can access the platform the user is using to do conditional Top or Bottom navigation
   return (
     <NavigationContainer>
-      <Tabs />
+      {/* <Tabs /> */}
+      <Counter />
     </NavigationContainer>
   )
 }
