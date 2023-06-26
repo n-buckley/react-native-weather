f1e8cdb187ffcc87be67bbc263a0c0a9342f2c25
n-buckley <buckley.ni@northeastern.edu>
2023-06-25 22:21:38 -0400

    add: imperial units

--- src/hooks/useGetWeather.js
+++ src/hooks/useGetWeather.js
@@ -11,7 +11,7 @@ export const useGetWeather = () => {
   const fetchWeatherData = async () => {
     try {
       const res = await fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${process.env.TEST_KEY}`
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${process.env.TEST_KEY}&units=imperial`
       )
       const data = await res.json()
       setWeather(data)
